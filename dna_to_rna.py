d65994e6b3c376d15d8ee04644e4e987696b1784
jorley-silva <jorley2552@gmail.com>
2023-03-07 16:02:58 -0300

    Lines

--- dna_to_rna.py
+++ dna_to_rna.py
@@ -8,6 +8,4 @@ with open("rosalind_rna.txt","r") as texto:            #Importing dataset
     dna = texto.read()                                  #Saving content to dna variable
 
 
-
-
 print(dna.replace("T","U"))
